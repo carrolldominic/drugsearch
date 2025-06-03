90ef5e53e478553f40e45038d88b73771f4f1ab3
carrolldominic <carroll.886@osu.edu>
2025-06-02 23:01:33 -0400

    tilde

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
     $('#searchForm').on('submit', function(e) {
-      e.preventDefault();
-      const drugName = $('#drugName').val();
-      window.location.href = `/drug?name=${encodeURIComponent(drugName)}`;
+        e.preventDefault();
+        const drugName = $('#drugName').val();
+        window.location.href = `/drug?name=${encodeURIComponent(drugName)}`;
     });
-  });
+
+    $(document).on('keydown', function(e) {
+        if (e.key === '`' || e.key === '~') {
+            e.preventDefault(); // Prevent default behavior of the '~' key
+            $('#drugName').focus().select(); // Focus and highlight the input
+        }
+    });
+});
